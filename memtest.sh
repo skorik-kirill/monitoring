09cb8da60e4ce454929d67d1cc4a11079cdf4b28
skorik-kirill <60388222+skorik-kirill@users.noreply.github.com>
2020-02-21 13:03:08 +0200

    Update memtest.sh

--- memtest.sh
+++ memtest.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 ramtotal=$(free | awk '/Mem/{print $2}')
 ramusage=$(free | awk '/Mem/{print $3}')
-let "test = $ramtotal / 2"
-echo "50% of memoty: $test";
+let " rampercent=(($ramtotal * 5) / 10)"
+echo "50% of memoty: $rampercent";
 echo "Total RAM: $ramtotal";
 echo "Usage RAM: $ramusage";
-if [[ "$ramusage" -gt "$test" ]]; then
+if [[ "$ramusage" -gt "$rampercent" ]]; then
 echo "critical ram state"
 else
 echo "OK state"
 fi
+
+
 
 
 
